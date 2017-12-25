39596aeffd0dc08b4ef763e1ceda22726ca5ee67
lmerengues <317958662@qq.com>
2017-12-25 17:23:10 +0800

    修改order

--- mp/order.py
+++ mp/order.py
@@ -22,25 +22,11 @@ def dictfetchall(cursor):
     	]
 
 def index(request):
-	cursor = connections['default'].cursor()	    
-    #return HttpResponse("Hello world ! ")
-	bno = request.GET['bno']
-	cursor.execute("select Orders.ono,ostatus,Seller.sno,sname,simg,otime from Orders,Seller where Orders.sno = Seller.sno and Orders.bno = %s and ostatus = 1",(bno,))
+	hno = request.GET['hno']
 
+	cursor = connections['default'].cursor()
+	cursor.execute("select hno,htitle1,htitle2,htitle3,hpic from house where house.hno = %s",(hno,))
 	raw = dictfetchall(cursor)
-	#now = datetime.utcnow().replace(tzinfo=<Asia/Shanghai>)
-	now = datetime.now(pytz.timezone('Asia/Shanghai'))
-	for item in raw:
-		item['otime'] = item['otime'].replace(tzinfo=pytz.timezone('Asia/Shanghai'))
-		item['oday'] = (now-item['otime']).days
-		item['now'] = json_serial(now)
-		item['osecond'] = (now-item['otime']).seconds
-		#item['ominute'] = (now-item['otime']).minutes
-		item['ohour'] =item['oday']*24+ ((now-item['otime']).seconds - item['oday']*24*60*60)/(60*60)
-		minutes = (item['osecond'] % 3600)
-		item['ominute'] = minutes - item['oday']*24*60 - item['ohour']*60
-		item['otime'] = json_serial(item['otime'])
-		
 	cursor.close()
 
 	response = HttpResponse(json.dumps(raw),content_type="application/json")	
