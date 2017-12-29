29e083a2486fb2abda4a85a3656958016e4e8d1f
lmerengues <317958662@qq.com>
2017-12-30 00:25:48 +0800

    ?

--- mp/order.py
+++ mp/order.py
@@ -10,6 +10,8 @@ def json_serial(obj):
 
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
+	if isinstance(obj, (datetime, time)):
+		return obj.isoformat()
     raise TypeError ("Type %s not serializable" % type(obj))
 
 #cursor = connections['default'].cursor()
