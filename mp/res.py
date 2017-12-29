c9892be80945821f0558a3dc7a74555cb8b84fb2
lmerengues <317958662@qq.com>
2017-12-30 01:17:53 +0800

    test

--- mp/res.py
+++ mp/res.py
@@ -2,6 +2,16 @@ from django.http import HttpResponse
 
 import json
 from django.db import connections
+from datetime import date, datetime,time
+
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date,time)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
 
 def dictfetchall(cursor):
 	desc = cursor.description
@@ -15,10 +25,14 @@ def index(request):
     openid = request.GET['openid']
 
     cursor = connections['default'].cursor()
-    cursor.execute("select oid,orders.hno,odate,ostart,oend,orders.ocno from orders,contact,house"
-                   "where orders.hno = house.hno and orders.ocno = contact.cno and orders.uno = %s order by orders.otime desc",(openid,))
+    cursor.execute("select oid,orders.hno,odate,ostart,oend,orders.ocno,otime from orders,contact,house"
+                   "where orders.hno = house.hno and orders.ocno = contact.cno and orders.uno = %s order by otime desc",(openid,))
     raw = dictfetchall(cursor)
     cursor.close()
+
+
+    for item in raw:
+        item['otime'] = json_serial(item['otime'])
 
     cursor = connections['default'].cursor()
     cursor.execute("select unickName,uavatarurl from Users where uid = %s",(openid,))
