f0107a730738b69f0e4fd6fab6e5b5c2516a8283
lmerengues <317958662@qq.com>
2017-12-30 19:06:20 +0800

    let's check

--- mp/res.py
+++ mp/res.py
@@ -43,10 +43,10 @@ def index(request):
 
         item['ototal'] = int(item['ototal'])/100
 
-        
+
     cursor = connections['default'].cursor()
     cursor.execute(
-        "select oid,orders.hno,odate,ostart,oend,onum,ototal,orders.ocno,hpic,htitle1,htitle2 from orders,contact,house where orders.hno = house.hno and orders.ocno = contact.cno and orders.uno = %s and ostatus = 1 and orders.odate>= sysdate() order by otime desc limit 1",
+        "select oid,orders.hno,odate,ostart,oend,onum,ototal,orders.ocno,hpic,htitle1,htitle2 from orders,contact,house where orders.hno = house.hno and orders.ocno = contact.cno and orders.uno = %s and ostatus = 1 and orders.odate>= curdate() order by odate asc,ostart asc",
         (openid,))
     latest_raw = dictfetchall(cursor)
     cursor.close()
