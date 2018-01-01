74cda64ad38335f2c23e124b2606c77f23f8612d
lmerengues <317958662@qq.com>
2018-01-02 02:06:52 +0800

    mm

--- myModel/admin.py
+++ myModel/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from myModel.models import Users,UsersLocation,Contact,Equip,House,HouseDiscount,\
-    HouseDisplay,HouseEquip,HouseIcon,HouseLabel,HouseMemory,HousePic,Icon,Orders
+#from myModel.models import Users,UsersLocation,Contact,Equip,House,HouseDiscount,\
+#    HouseDisplay,HouseEquip,HouseIcon,HouseLabel,HouseMemory,HousePic,Icon,Orders
 
+from myModel import models as mm
 # Register your models here.
 #admin.site.register([Users, UsersLocation, Contact,Equip,HouseMemory,HousePic,Icon,Orders,
 #                     House,HouseDiscount,HouseDisplay,HouseEquip,HouseIcon,HouseLabel])
 
 class UsersAdmin(admin.ModelAdmin):
-    list_display = [f.name for f in Users._meta.fields]
+    list_display = [f.name for f in mm.Users._meta.fields]
 
-admin.site.register(Users,UsersAdmin)
+admin.site.register(mm.Users,UsersAdmin)
