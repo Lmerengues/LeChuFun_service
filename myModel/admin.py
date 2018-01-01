97ae1eb2fa7fec05333cac4069f1624b20a0987a
lmerengues <317958662@qq.com>
2018-01-02 01:51:33 +0800

    register

--- myModel/admin.py
+++ myModel/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from myModel.models import Users,UsersLocation,Contact,Equip,House,HouseDiscount,\
+    HouseDisplay,HouseEquip,HouseIcon,HouseLabel,HouseMemory,HousePic,Icon,Orders
 
 # Register your models here.
+admin.site.register([Users, UsersLocation, Contact,Equip,HouseMemory,HousePic,Icon,Orders,
+                     House,HouseDiscount,HouseDisplay,HouseEquip,HouseIcon,HouseLabel])
+
