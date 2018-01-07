a03b6ac1c782e02cd9ac0f112a05998a24d680da
lmerengues <317958662@qq.com>
2018-01-07 20:01:05 +0800

    test

--- mp/view.py
+++ mp/view.py
@@ -28,7 +28,7 @@ def add(request):
     context          = {}
     #context['hello'] = 'Hello World!'
 
-    '''
+
     cursor = connections['default'].cursor()
     cursor.execute("select * from equip")
     eraw = dictfetchall(cursor)
@@ -40,7 +40,7 @@ def add(request):
     cursor.close()
     context['equips'] = eraw
     context['icons'] = iraw
-    '''
+    
     context['test'] = 'hello'
 
     return render(request, 'add.html', context)
