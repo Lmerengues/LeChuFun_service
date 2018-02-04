a974f900ea9ff6b3310f54b7200dcf00d631eefe
lmerengues <317958662@qq.com>
2018-02-04 16:15:41 +0800

    kindex add activity_rank_hot

--- mp/k_index.py
+++ mp/k_index.py
@@ -15,12 +15,22 @@ def dictfetchall(cursor):
 
 def index(request):
 
+    raw = {}
     cursor = connections['klook'].cursor()
     cursor.execute(
         "select place_hot.pno,ptitle,purl from place,place_hot where place.pno = place_hot.pno order by pval desc")
-    raw = dictfetchall(cursor)
+    raw['hot_place'] = dictfetchall(cursor)
     cursor.close()
+
+    cursor = connections['klook'].cursor()
+    cursor.execute(
+        "select activity_rank_hot.ano,atitle1,anum,ascore,aprice,aprice_old,ahour,aurl from activities,activity_rank_hot where activities.ano = activity_rank_hot.ano order by aval desc")
+    raw['hot_acti'] = dictfetchall(cursor)
+    cursor.close()
+
+
+
+
 
     response = HttpResponse(json.dumps(raw), content_type="application/json")
     return response
-
