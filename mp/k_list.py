82a856a6b3997473688729574ebba87e80d22a97
lmerengues <317958662@qq.com>
2018-02-07 16:27:03 +0800

    add k_list

---
+++ mp/k_list.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+
+import json
+from django.db import connections
+
+from datetime import date, datetime,time
+
+import math
+
+def dictfetchall(cursor):
+	desc = cursor.description
+	return [
+	dict(zip([col[0] for col in desc], row))
+    	for row in cursor.fetchall()
+    	]
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date,date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+def index(request):
+    pno = request.GET['pno']
+
+    raw = {}
+
+    cursor = connections['klook'].cursor()
+    cursor.execute("select ano,atitle1,anum,ascore,aprice,aprice_old,ahour,adate,aurl,ptitle from activities,place,activity_type where activities.pno = %s and activities.pno = place.pno and activities.ano = activity_type.ano and tno = 1 order by anum desc",(pno,))
+    raw['list'] = dictfetchall(cursor)
+    cursor.close()
+
+    response = HttpResponse(json.dumps(raw), content_type="application/json")
+    return response
