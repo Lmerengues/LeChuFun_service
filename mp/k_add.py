7387231f64524a0785f2c60b2eb6b2a39a7f02c2
lmerengues <317958662@qq.com>
2018-02-18 21:12:27 +0800

    debug

--- mp/k_add.py
+++ mp/k_add.py
@@ -12,7 +12,7 @@ def add_activity(request):
     basic_url = 'https://mina.mapglory.com/static/images/canaan/5.jpeg'
 
     cursor = connections['klook'].cursor()
-    cursor.execute("insert into activities values(null,%s,%s,%s,%s,0,%s,%s,%s,%s,%s,'',0,%s,sysdate())",(request.POST['atitle1'],request.POST['atitle2'],request.POST['aprice'],request.POST['aprice_old'],request.POST['adate'],request.POST['ahour'],request.POST['adetail'],request.POST['alongitude'],request.POST['alatitude'],basic_url,request.POST['pno'],))
+    cursor.execute("insert into activities values(null,%s,%s,%s,%s,0,%s,%s,%s,%s,%s,%s,0,%s,sysdate())",(request.POST['atitle1'],request.POST['atitle2'],request.POST['aprice'],request.POST['aprice_old'],request.POST['adate'],request.POST['ahour'],request.POST['adetail'],request.POST['alongitude'],request.POST['alatitude'],basic_url,request.POST['pno'],))
     cursor.close()
 
     raw = {'status':1}
