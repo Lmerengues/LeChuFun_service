fdc0c0bcc59c23d0fa6106f93b0719c1698c2a28
lmerengues <317958662@qq.com>
2018-02-18 21:19:00 +0800

    debug

--- mp/k_add.py
+++ mp/k_add.py
@@ -12,10 +12,12 @@ def add_activity(request):
     basic_url = 'https://mina.mapglory.com/static/images/canaan/5.jpeg'
 
     cursor = connections['klook'].cursor()
-    cursor.execute("insert into activities values(null,%s,%s,%s,%s,0,%s,%s,%s,%s,%s,%s,0,%s,sysdate())",(request.POST['atitle1'],request.POST['atitle2'],request.POST['aprice'],request.POST['aprice_old'],request.POST['adate'],request.POST['ahour'],request.POST['adetail'],request.POST['alongitude'],request.POST['alatitude'],basic_url,request.POST['pno'],))
+    #cursor.execute("insert into activities values(null,%s,%s,%s,%s,0,%s,%s,%s,%s,%s,%s,0,%s,sysdate())",(request.POST['atitle1'],request.POST['atitle2'],request.POST['aprice'],request.POST['aprice_old'],request.POST['adate'],request.POST['ahour'],request.POST['adetail'],request.POST['alongitude'],request.POST['alatitude'],basic_url,request.POST['pno'],))
+    cursor.execute("insert into activities values(null,'1','1','1','1',0,'2018-06-05','1','1','1','1','1',0,'0',sysdate())")
     cursor.close()
 
     raw = {'status':1}
 
     resp = HttpResponse(json.dumps(raw), content_type="application/json")
     return resp
+
