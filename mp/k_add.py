4bbf5c7474194bbab990b2b86ec801d2f4292789
lmerengues <317958662@qq.com>
2018-03-14 09:07:49 +0800

    add random_base

--- mp/k_add.py
+++ mp/k_add.py
@@ -7,6 +7,7 @@ from datetime import date, datetime,time
 
 import math
 import os
+import random
 
 from django.core.files.storage import default_storage
 from django.conf import settings
@@ -24,9 +25,10 @@ def add_activity(request):
 
     basic_url = 'https://mina.mapglory.com/static/images/canaan/5.jpeg'
 
+    base_num  = random.randint(10, 50)
     cursor = connections['klook'].cursor()
     #cursor.execute("insert into activities values(null,%s,%s,%s,%s,0,%s,%s,%s,%s,%s,%s,0,%s,sysdate())",(request.POST['atitle1'],request.POST['atitle2'],request.POST['aprice'],request.POST['aprice_old'],request.POST['adate'],request.POST['ahour'],request.POST['adetail'],request.POST['alongitude'],request.POST['alatitude'],basic_url,request.POST['pno'],))
-    cursor.execute("insert into activities values(null,%s,%s,%s,%s,0,%s,%s,%s,%s,%s,%s,0,%s,sysdate())",(request.POST['atitle1'],request.POST['atitle2'],request.POST['aprice'],request.POST['aprice_old'],request.POST['adate'],request.POST['ahour'],request.POST['adetail'],request.POST['hlongitude'],request.POST['hlatitude'],basic_url,request.POST['pno'],))
+    cursor.execute("insert into activities values(null,%s,%s,%s,%s,base_num,%s,%s,%s,%s,%s,%s,0,%s,sysdate())",(request.POST['atitle1'],request.POST['atitle2'],request.POST['aprice'],request.POST['aprice_old'],request.POST['adate'],request.POST['ahour'],request.POST['adetail'],request.POST['hlongitude'],request.POST['hlatitude'],basic_url,request.POST['pno'],))
     cursor.close()
 
     cursor = connections['klook'].cursor()
